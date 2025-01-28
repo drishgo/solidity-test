5b8f6c3238dd33c33e9cac844b29407ab3a247c9
drishgo <drishgo@gmail.com>
2025-01-28 18:24:20 +0530

    testingContractSending

--- contracts/FundMe/FundMe.sol
+++ contracts/FundMe/FundMe.sol
@@ -5,19 +5,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 contract FundMe{
-    uint256 public minimumUSD = 25;
+    using PriceConverter for uint256;
+    uint256 public minimumUSD = 25*1e18;
     
+    address[] public  patrons;
+    mapping(address=> uint256) public fundingGivenByAddress;
 
     function fund() public payable {
-    
 
-               require(msg.value>=minimumUSD,"The funding does not meet base requirement ");
-       
+               require(msg.value.getConversionRate()>=minimumUSD,"The funding does not meet base requirement ");
+                patrons.push(msg.sender);   
+                fundingGivenByAddress[msg.sender]=msg.value;
     }
+     
 
 
-    // function Withdraw(){
 
-    // }
+    function Withdraw() public {
+        for(uint256 i = 0;i<patrons.length; i++){
+            address patron = patrons[i];
+            fundingGivenByAddress[patron] = 0;
+    }
+    require(payable(msg.sender).send(address(this).balance),"Could not send Try again");
+}
 }

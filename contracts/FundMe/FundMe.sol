1cc0d7b65c190e23354bdc7e841fb58223700735
drishgo <drishgo@gmail.com>
2025-01-28 19:47:42 +0530

    gas optimizations

--- contracts/FundMe/FundMe.sol
+++ contracts/FundMe/FundMe.sol
@@ -5,12 +5,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+
+//gas cost: 850,193 gas
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
+error NotOwner();
 contract FundMe{
+
     using PriceConverter for uint256;
-    uint256 public minimumUSD = 25*1e18;
-    
+    uint256 public constant minimumUSD = 25*1e18; //constant defined vars dont take up storage spot 
+    //new gas: 827,270
     address[] public  patrons;
     mapping(address=> uint256) public fundingGivenByAddress;
     /*
@@ -18,14 +22,15 @@ contract FundMe{
     IT IS USED FOR CREATING A NEW INSTANCE OF A GIVEN CONTRACT RATHER THAN CREATING
     INSTANCE OF OBJECT.
     */
-    address public owner;
+    address public immutable i_owner;
+    //new gas: 800624
     constructor(){
             /*
                 when a new instance of the contract is deployed, the constructor is
                 automatically called and the code inside of the constructor is automatically 
                 executed first, much like constructors in Java.
             */
-            owner = msg.sender;
+            i_owner = msg.sender;
     }
 
     function fund() public payable {
@@ -36,7 +41,10 @@ contract FundMe{
     }
 
     modifier onlyOwner {
-        require(msg.sender==owner,"Cannot withdraw");
+        // require(msg.sender==i_owner,"Cannot withdraw");
+        if(msg.sender!=i_owner){
+            revert NotOwner();
+        }
         _;
     /* The _; represents the function code like withdrow is onlyOwner so when
         withdraw is tagged with onlyOwner, the _; represents the code of withdraw.
@@ -55,7 +63,13 @@ contract FundMe{
     patrons = new address[](0);
     require(payable(msg.sender).send(address(this).balance),"Could not send Try again");
 }
+    receive() external payable {
+        fund();
 
+     }
+     fallback() external payable {
+        fund();
+      }
 }
 
 
@@ -71,3 +85,6 @@ contract FundMe{
    ASS NETWORK COZ THE NETWORK FEES IS TOO FUCKING HIGH MAN WTF
     DOG
 */
+
+
+//  RECIEVE - recieve()

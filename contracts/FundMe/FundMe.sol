a31a1001ba2c3ff97fa62c783533df6d5c10d138
drishgo <drishgo@gmail.com>
2025-01-28 19:04:35 +0530

    added ownership based withdrawal

--- contracts/FundMe/FundMe.sol
+++ contracts/FundMe/FundMe.sol
@@ -13,6 +13,20 @@ contract FundMe{
     
     address[] public  patrons;
     mapping(address=> uint256) public fundingGivenByAddress;
+    /*
+    CONSTRUCTOR IN SOLIDITY IS SAME AS CONSTRUCTOR IN JAVA, BUT IN THIS CASE,
+    IT IS USED FOR CREATING A NEW INSTANCE OF A GIVEN CONTRACT RATHER THAN CREATING
+    INSTANCE OF OBJECT.
+    */
+    address public owner;
+    constructor(){
+            /*
+                when a new instance of the contract is deployed, the constructor is
+                automatically called and the code inside of the constructor is automatically 
+                executed first, much like constructors in Java.
+            */
+            owner = msg.sender;
+    }
 
     function fund() public payable {
 
@@ -20,11 +34,20 @@ contract FundMe{
                 patrons.push(msg.sender);   
                 fundingGivenByAddress[msg.sender]=msg.value;
     }
-     
+
+    modifier onlyOwner {
+        require(msg.sender==owner,"Cannot withdraw");
+        _;
+    /* The _; represents the function code like withdrow is onlyOwner so when
+        withdraw is tagged with onlyOwner, the _; represents the code of withdraw.
+    */
+      }
 
 
 
-    function Withdraw() public {
+    function Withdraw() public onlyOwner {
+        //Setting condition that only the contract owner can withdraw eth.
+        
         for(uint256 i = 0;i<patrons.length; i++){
             address patron = patrons[i];
             fundingGivenByAddress[patron] = 0;
@@ -32,6 +55,7 @@ contract FundMe{
     patrons = new address[](0);
     require(payable(msg.sender).send(address(this).balance),"Could not send Try again");
 }
+
 }
 
 
@@ -40,3 +64,10 @@ contract FundMe{
 // A PAYABLE PARAMETER ALLOWS A FUNCTION AND THEREFORE A CONTRACT TO BE ABLE TO
 // PERFORM ETHER TRANSFER TO AND FROM AN EOA LIKE METAMASK.
 // SO THE FUNCTION fund() IS DECLARED TO BE PAYABLE.
+
+
+
+/* AT THE TIME OF WRITING THIS CONTRACT NONE OF IT WOULD GET DEOPLYED ON THIS SHITTY
+   ASS NETWORK COZ THE NETWORK FEES IS TOO FUCKING HIGH MAN WTF
+    DOG
+*/
